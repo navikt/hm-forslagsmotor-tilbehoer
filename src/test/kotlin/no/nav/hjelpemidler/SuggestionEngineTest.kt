db2dfa5bcf0ebc97c9359e27c7284743d6933ecf
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2021-09-16 16:20:51 +0200

    Require atleast five occurances of accessory use before suggesting it

--- src/test/kotlin/no/nav/hjelpemidler/SuggestionEngineTest.kt
+++ src/test/kotlin/no/nav/hjelpemidler/SuggestionEngineTest.kt
@@ -15,7 +15,7 @@ internal class SuggestionEngineTest {
     }
 
     @Test
-    fun `A single suggestion is available`() {
+    fun `Fewer occurances than five results in no result, five or more occurances results in results`() {
         SuggestionEngine.discardDataset()
         SuggestionEngine.learnFromSoknad(
             listOf(
@@ -28,7 +28,155 @@ internal class SuggestionEngineTest {
                             navn = "Tilbehør 1",
                         )
                     ),
-                )
+                ),
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        )
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        )
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        )
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "54321",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "12345",
+                            antall = 1,
+                            navn = "Tilbehør 2",
+                        )
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "54321",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "12345",
+                            antall = 1,
+                            navn = "Tilbehør 2",
+                        )
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "54321",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "12345",
+                            antall = 1,
+                            navn = "Tilbehør 2",
+                        )
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "54321",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "12345",
+                            antall = 1,
+                            navn = "Tilbehør 2",
+                        )
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "54321",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "12345",
+                            antall = 1,
+                            navn = "Tilbehør 2",
+                        )
+                    ),
+                ),
+            )
+        )
+
+        val suggestions = SuggestionEngine.suggestionsForHmsNr("1234")
+        assertEquals(0, suggestions.size)
+
+        val suggestions2 = SuggestionEngine.suggestionsForHmsNr("54321")
+        assertEquals(1, suggestions2.size)
+        assertEquals("123451", suggestions2[0].hmsNr)
+        assertEquals("(beskrivelse utilgjengelig)", suggestions2[0].title)
+        assertEquals(5, suggestions2[0].occurancesInSoknader)
+    }
+
+    @Test
+    fun `A single suggestion is available based on five or more occurances`() {
+        SuggestionEngine.discardDataset()
+        SuggestionEngine.learnFromSoknad(
+            listOf(
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        )
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        )
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        )
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        )
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        )
+                    ),
+                ),
             )
         )
 
@@ -36,7 +184,7 @@ internal class SuggestionEngineTest {
         assertEquals(1, suggestions.size)
         assertEquals("4321", suggestions[0].hmsNr)
         assertEquals("(beskrivelse utilgjengelig)", suggestions[0].title)
-        assertEquals(1, suggestions[0].occurancesInSoknader)
+        assertEquals(5, suggestions[0].occurancesInSoknader)
     }
 
     @Test
@@ -58,7 +206,67 @@ internal class SuggestionEngineTest {
                             navn = "Tilbehør 2",
                         ),
                     ),
-                )
+                ),
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        ),
+                        Tilbehoer(
+                            hmsnr = "5678",
+                            antall = 1,
+                            navn = "Tilbehør 2",
+                        ),
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        ),
+                        Tilbehoer(
+                            hmsnr = "5678",
+                            antall = 1,
+                            navn = "Tilbehør 2",
+                        ),
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        ),
+                        Tilbehoer(
+                            hmsnr = "5678",
+                            antall = 1,
+                            navn = "Tilbehør 2",
+                        ),
+                    ),
+                ),
+                Hjelpemiddel(
+                    hmsNr = "1234",
+                    tilbehorListe = listOf(
+                        Tilbehoer(
+                            hmsnr = "4321",
+                            antall = 1,
+                            navn = "Tilbehør 1",
+                        ),
+                        Tilbehoer(
+                            hmsnr = "5678",
+                            antall = 1,
+                            navn = "Tilbehør 2",
+                        ),
+                    ),
+                ),
             )
         )
 
@@ -74,13 +282,13 @@ internal class SuggestionEngineTest {
                         ),
                     ),
                 )
-            )
+            ),
         )
 
         val suggestions = SuggestionEngine.suggestionsForHmsNr("1234")
         assertEquals(2, suggestions.size)
         assertEquals("5678", suggestions[0].hmsNr)
         assertEquals("(beskrivelse utilgjengelig)", suggestions[0].title)
-        assertEquals(2, suggestions[0].occurancesInSoknader)
+        assertEquals(6, suggestions[0].occurancesInSoknader)
     }
 }
