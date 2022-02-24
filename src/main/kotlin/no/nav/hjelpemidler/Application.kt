225ed82c0f662979650907a47cfecbcd1c01e243
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-02-24 10:29:02 +0100

    Divide Application.kt into two (separate out Routes.kt)

--- src/main/kotlin/no/nav/hjelpemidler/Application.kt
+++ src/main/kotlin/no/nav/hjelpemidler/Application.kt
@@ -10,27 +10,19 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.jackson.JacksonConverter
-import io.ktor.response.respond
 import io.ktor.response.respondRedirect
 import io.ktor.response.respondText
-import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import mu.KotlinLogging
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.hjelpemidler.configuration.Configuration
-import no.nav.hjelpemidler.db.SoknadStore
 import no.nav.hjelpemidler.db.SoknadStorePostgres
 import no.nav.hjelpemidler.db.dataSource
 import no.nav.hjelpemidler.db.migrate
 import no.nav.hjelpemidler.db.waitForDB
-import no.nav.hjelpemidler.model.ProductFrontendFiltered
-import no.nav.hjelpemidler.model.SuggestionsFrontendFiltered
-import no.nav.hjelpemidler.oebs.Oebs
 import no.nav.hjelpemidler.rivers.NySøknadInnsendt
-import no.nav.hjelpemidler.soknad.db.client.hmdb.HjelpemiddeldatabaseClient
-import kotlin.system.measureTimeMillis
 import kotlin.time.ExperimentalTime
 import kotlin.time.minutes
 
@@ -97,114 +89,4 @@ fun main() {
         }.start()
 
     logg.debug("Debug: After rapid start, end of main func")
-}
-
-fun Route.ktorRoutes(store: SoknadStore) {
-    // authenticate("tokenX", "aad") {}
-
-    get("/suggestions/{hmsNr}") {
-        val hmsnr = call.parameters["hmsnr"]!!
-
-        logg.info("Request for suggestions for hmsnr=$hmsnr.")
-        val suggestions = store.suggestions(hmsnr)
-
-        val hmsNrsSkipList = HjelpemiddeldatabaseClient
-            .hentProdukterMedHmsnrs(suggestions.suggestions.map { it.hmsNr }.toSet())
-            .filter { it.hmsnr != null && it.tilgjengeligForDigitalSoknad }
-            .map { it.hmsnr!! }
-
-        val results = SuggestionsFrontendFiltered(
-            suggestions.dataStartDate,
-            suggestions.suggestions.filter { !hmsNrsSkipList.contains(it.hmsNr) }
-                .map { it.toFrontendFiltered() },
-        )
-
-        call.respond(results)
-    }
-
-    get("/lookup-accessory-name/{hmsNr}") {
-        val hmsnr = call.parameters["hmsNr"]!!
-
-        data class LookupAccessoryName(
-            val name: String?,
-            val error: String?,
-        )
-
-        logg.info("Request for name lookup for hmsnr=$hmsnr.")
-        runCatching {
-            // Søknaden er avhengig av denne gamle sjekken, da den egentlig sjekker om produktet eksisterer i hmdb
-            // og hvis så om den er tilgjengelig for å legges til igjennom digital søknad som hovedprodukt.
-            var accessory = true
-            val hmdbResults = HjelpemiddeldatabaseClient.hentProdukterMedHmsnr(hmsnr)
-            if (hmdbResults.any { it.tilgjengeligForDigitalSoknad }) {
-                logg.info("DEBUG: product looked up with /lookup-accessory-name was not really an accessory")
-                accessory = false
-            }
-
-            val titleFromSuggestionEngineCache = store.cachedTitleAndTypeFor(hmsnr)
-            var oebsTitleAndType: Pair<String, String>? = null
-            if (titleFromSuggestionEngineCache?.title == null) {
-                oebsTitleAndType = Oebs.getTitleForHmsNr(hmsnr)
-                logg.info("DEBUG: Fetched title for $hmsnr and oebs report it as having type: ${oebsTitleAndType.second}. Title: ${oebsTitleAndType.first}")
-                if (oebsTitleAndType.second != "Del") {
-                    logg.info("DEBUG: $hmsnr is not a \"DEl\" according to OEBS (type=${oebsTitleAndType.second}; title=${oebsTitleAndType.first})")
-                    // accessory = false
-                }
-            } else {
-                logg.info("DEBUG: Using cached oebs title from suggestion engine for $hmsnr: $titleFromSuggestionEngineCache")
-            }
-
-            call.respond(
-                LookupAccessoryName(
-                    oebsTitleAndType?.first ?: titleFromSuggestionEngineCache?.title,
-                    if (!accessory) {
-                        "ikke et tilbehør"
-                    } else {
-                        null
-                    }
-                )
-            )
-        }.getOrElse { e ->
-            logg.info("warn: failed to find title for hmsNr=$hmsnr")
-            e.printStackTrace()
-            call.respond(LookupAccessoryName(null, "produkt ikke funnet"))
-        }
-    }
-
-    get("/introspect") {
-        var result: List<ProductFrontendFiltered>?
-        val timeElapsed = measureTimeMillis {
-            result = store.introspect()
-
-            // Filter out a distinct list of hmsnrs for accessories that could be suggested by the suggestion engine
-            val allSuggestionHmsnrs = result!!
-                .map { it.suggestions }
-                .fold(mutableListOf<String>()) { a, b ->
-                    a.addAll(b.map { it.hmsNr })
-                    a
-                }.toSet()
-
-            // Talk to hm-grunndata about a skip list
-            val hmsNrsSkipList = HjelpemiddeldatabaseClient
-                .hentProdukterMedHmsnrs(allSuggestionHmsnrs)
-                .filter { it.hmsnr != null && it.tilgjengeligForDigitalSoknad }
-                .map { it.hmsnr!! }
-
-            // Filter out illegal suggestions because they are not accessories (they can be applied to digitally as products)
-            result = result!!.map {
-                ProductFrontendFiltered(
-                    hmsnr = it.hmsnr,
-                    title = it.title,
-                    frameworkAgreementStartDate = it.frameworkAgreementStartDate,
-                    suggestions = it.suggestions.filter { !hmsNrsSkipList.contains(it.hmsNr) },
-                )
-            }
-        }
-
-
-        logg.info("Request for introspection of suggestions (timeElapsed=${timeElapsed}ms)")
-        call.respond(result!!)
-    }
-
-    get("/inspection") { call.respondRedirect("/introspect") }
 }
