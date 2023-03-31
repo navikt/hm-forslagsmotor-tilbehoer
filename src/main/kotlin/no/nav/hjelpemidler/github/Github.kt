603d636686fdacc1557c0f7b9276c08b987cc0bf
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2023-03-31 11:01:23 +0200

    opprydding ++

--- src/main/kotlin/no/nav/hjelpemidler/github/Github.kt
+++ src/main/kotlin/no/nav/hjelpemidler/github/Github.kt
@@ -5,13 +5,11 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import mu.KotlinLogging
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-private val logger = KotlinLogging.logger {  }
 
 object Github {
     private val objectMapper = jacksonObjectMapper()
@@ -29,9 +27,14 @@ object Github {
             .GET()
             .build()
 
-        val response: HttpResponse<String> = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString())
+        val response: HttpResponse<String> =
+            HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString())
         if (response.statusCode() != 200) {
-            throw Exception("error: unexpected status code from github (statusCode=${response.statusCode()} headers=${response.headers()} body[:40]=${response.body().take(40)})")
+            throw Exception(
+                "error: unexpected status code from github (statusCode=${response.statusCode()} headers=${response.headers()} body[:40]=${
+                    response.body().take(40)
+                })"
+            )
         }
 
         return objectMapper.readValue(response.body())
@@ -45,9 +48,14 @@ object Github {
             .GET()
             .build()
 
-        val response: HttpResponse<String> = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString())
+        val response: HttpResponse<String> =
+            HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString())
         if (response.statusCode() != 200) {
-            throw Exception("error: unexpected status code from github (statusCode=${response.statusCode()} headers=${response.headers()} body[:40]=${response.body().take(40)})")
+            throw Exception(
+                "error: unexpected status code from github (statusCode=${response.statusCode()} headers=${response.headers()} body[:40]=${
+                    response.body().take(40)
+                })"
+            )
         }
 
         return objectMapper.readValue<Rammeavtaler>(response.body())
