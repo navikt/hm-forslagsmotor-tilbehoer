bcb69607e905036caaf58ed8b1d770a6c9c8f174
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2023-04-19 08:57:56 +0200

    Legg til standard sengegrind til Opus K85DW på denylist

--- src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
+++ src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
@@ -2,6 +2,8 @@ package no.nav.hjelpemidler
 
 val denyList = setOf(
     // Sengegrinder til Opus elektrisk seng. Følger med som del av hovedproduktet. Ingen grunn til at man skal trenge å søke digitalt om dette
-    "213163",
-    "213164",
+    "214192", // Opus K85DW
+    "214193", // Opus K85DW
+    "213163", // Opus 90DW
+    "213164", // Opus 90DW
 )
