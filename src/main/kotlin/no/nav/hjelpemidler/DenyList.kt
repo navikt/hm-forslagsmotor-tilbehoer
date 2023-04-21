4735dc0fb025078afd72d0b51d7ed0a4ed58efc6
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2023-04-21 11:18:50 +0200

    fix hmsnr for standardsengegrind til opus 90/120

--- src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
+++ src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
@@ -1,9 +1,5 @@
 package no.nav.hjelpemidler
 
 val denyList = setOf(
-    // Sengegrinder til Opus elektrisk seng. Følger med som del av hovedproduktet. Ingen grunn til at man skal trenge å søke digitalt om dette
-    "214192", // Opus K85DW
-    "214193", // Opus K85DW
-    "213163", // Opus 90DW
-    "213164", // Opus 90DW
+    "215124", "215125", // Standardgrinder til Opus 90 og 120 bredde, som følger med hovedhjelpemiddelet
 )
