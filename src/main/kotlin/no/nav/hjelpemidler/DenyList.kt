646019a59c9335f9e10cccdca2ca4930035a909e
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2023-04-21 14:14:19 +0200

    Svartelist standardutstyr krykkeholder til Invacare ers

--- src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
+++ src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
@@ -2,4 +2,5 @@ package no.nav.hjelpemidler
 
 val denyList = setOf(
     "215124", "215125", // Standardgrinder til Opus 90 og 120 bredde, som følger med hovedhjelpemiddelet
+    "167689", "203278", // Standardutstyr Holder krykke ers Orion Pro 4W/Comet Alpine+/Comet Ultra
 )
