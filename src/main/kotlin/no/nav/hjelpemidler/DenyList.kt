65f3e4ef45f37c38c6081ed20df1bef75b28ed3d
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2023-05-09 15:44:26 +0200

    Legg til '292412 Rensevæske personløfter badekar Bellavita' i denylist

--- src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
+++ src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
@@ -3,4 +3,5 @@ package no.nav.hjelpemidler
 val denyList = setOf(
     "215124", "215125", // Standardgrinder til Opus 90 og 120 bredde, som følger med hovedhjelpemiddelet
     "167689", "203278", // Standardutstyr Holder krykke ers Orion Pro 4W/Comet Alpine+/Comet Ultra
+    "292412", // Rensevæske personløfter badekar Bellavita el (forbruksvare som ikke dekkes av NAV)
 )
