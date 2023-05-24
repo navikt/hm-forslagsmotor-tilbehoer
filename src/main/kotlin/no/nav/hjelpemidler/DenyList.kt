88fd4f4c9523201164de9507d4a2250b83e171f0
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2023-05-24 18:16:34 +0200

    Legg til 189518 i DenyList

--- src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
+++ src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
@@ -4,4 +4,5 @@ val denyList = setOf(
     "215124", "215125", // Standardgrinder til Opus 90 og 120 bredde, som følger med hovedhjelpemiddelet
     "167689", "203278", // Standardutstyr Holder krykke ers Orion Pro 4W/Comet Alpine+/Comet Ultra
     "292412", // Rensevæske personløfter badekar Bellavita el (forbruksvare som ikke dekkes av NAV)
+    "189518", // Gripo Spesial for takhøyde <210cm. Finnes ikke på rammeavtale og blir dermed ikke stoppet som hovedhjelpemiddel
 )
