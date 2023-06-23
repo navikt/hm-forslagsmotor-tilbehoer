b821fe2d0d1307d5c45698191ecbbe32296c3b5c
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2023-06-23 14:30:07 +0200

    Svartelist 296408 'Ryggsekk ers Eloflex'

--- src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
+++ src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
@@ -6,4 +6,5 @@ val denyList = setOf(
     "292412", // Rensevæske personløfter badekar Bellavita el (forbruksvare som ikke dekkes av NAV)
     "189518", // Gripo Spesial for takhøyde <210cm. Finnes ikke på rammeavtale og blir dermed ikke stoppet som hovedhjelpemiddel
     "244433", // 'Monteringssett toalett Aquatec 9000 vegghengt' Aquatec forhøyer som ikke er på avtale lenger
+    "296408", // 'Ryggsekk ers Eloflex' - Dette er en standard sekk NAV ikke dekker.
 )
