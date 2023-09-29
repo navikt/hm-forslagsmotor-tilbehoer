f98c62f09fa409ea605d3b68eda8426fb462a4ab
Ole Steinar Lillestøl Skrede <3756410+olesls@users.noreply.github.com>
2023-09-29 10:53:32 +0200

    Update DenyList.kt

--- src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
+++ src/main/kotlin/no/nav/hjelpemidler/DenyList.kt
@@ -8,4 +8,5 @@ val denyList = setOf(
     "244433", // 'Monteringssett toalett Aquatec 9000 vegghengt' Aquatec forhøyer som ikke er på avtale lenger
     "296408", // 'Ryggsekk ers Eloflex' - Dette er en standard sekk NAV ikke dekker.
     "144149", // Hjelpemiddel: Glidelaken Arcus Vita glideflate
+    "214262", // Krevert tilleggsskjema. Individuell tilpasning timespris ny/brukt varmehjm Multishell/Dartex/Tynset/Mikko/HeliSki/Roller/Tolga/Tallberg/Handy Aktiv/HeliSki jr./ Goretex jr. /Budor/Trysil/Herkules/Monty/Splitt
 )
