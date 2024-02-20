089629b56b3eeb09fd7a489cea09b4d48af2f787
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2024-02-20 14:33:59 +0100

    fix utvalg i visning av forslag til forslagsmotor-frontend

--- src/main/kotlin/no/nav/hjelpemidler/suggestions/SuggestionEngine.kt
+++ src/main/kotlin/no/nav/hjelpemidler/suggestions/SuggestionEngine.kt
@@ -142,7 +142,7 @@ internal class SuggestionEnginePostgres(
                 suggestions = it.value.fold(mutableListOf<Suggestion>()) { a, b ->
                     a.add(b.second.third)
                     a
-                }.takeLast(MAX_NUMBER_OR_RESULTS).reversed(),
+                }.take(MAX_NUMBER_OR_RESULTS),
                 frameworkAgreementStartDate = it.value.firstOrNull()?.second?.second,
             )
         }.sortedBy { it.hmsnr }
